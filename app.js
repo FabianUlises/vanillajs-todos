e0c26c7a1f5fa4078b3b44d86edd25ff4a815d27
FabianUlises <ulisesfab.car@outlook.com>
2024-12-11 01:24:07 -0800

    Updated app js file. Created function to run when submit todo button is clicked. Function will create new object using user input.

--- app.js
+++ app.js
@@ -1,4 +1,18 @@
-console.log('hello from javascript');
+// Element Selectors
 const handleTodoSubmit = document.querySelector('#todoBtn');
-const newTodoInput = document.querySelector('.newTodoInput');
-handleTodoSubmit.addEventListener('click', (e) => console.log('button clicked'));
+const newTodoInput = document.querySelector('#newTodoInput');
+const todoList = document.querySelector('.todo-list');
+// Function to create new todo
+const createNewTodo = () => {
+    // Getting value from user input
+    const todoInput = newTodoInput.value;
+    // Creating user Object
+    const newTodo = {
+        todo: todoInput,
+        todoId: Math.floor(Math.random() * 8000),
+        isEditing: false,
+        isComplete: false
+    };
+}
+
+handleTodoSubmit.addEventListener('click', createNewTodo);
