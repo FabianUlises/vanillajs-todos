af1b624740527f473692e492ca82f4245db66d71
FabianUlises <ulisesfab.car@outlook.com>
2024-12-11 01:06:22 -0800

    Updated index html and app js file. Added a class name to todo input to be able to select it on js file. On js file I added element selectors & also added an event listener to the todo submit button.

---
+++ app.js
@@ -0,0 +1,4 @@
+console.log('hello from javascript');
+const handleTodoSubmit = document.querySelector('#todoBtn');
+const newTodoInput = document.querySelector('.newTodoInput');
+handleTodoSubmit.addEventListener('click', (e) => console.log('button clicked'));
