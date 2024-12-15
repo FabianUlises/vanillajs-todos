4f351a3d3bc0175b09f5dd8d54b05081935c96ea
FabianUlises <ulisesfab.car@outlook.com>
2024-12-14 23:40:47 -0800

    Updated app js file. Working on creating html elements for task.

--- app.js
+++ app.js
@@ -1,11 +1,18 @@
 // Element Selectors
 const handleTodoSubmit = document.querySelector('#todoBtn');
 const newTodoInput = document.querySelector('#newTodoInput');
-const todoList = document.querySelector('.todo-list');
+const todoList = document.querySelector('.todo__list');
 // Function to create new todo
-const createNewTodo = () => {
+const createNewTodo = (e) => {
+    e.preventDefault();
+    // Checking if input is valid
+    if(!newTodoInput.value || newTodoInput.value === '') {
+        return;
+    }
     // Getting value from user input
     const todoInput = newTodoInput.value;
+    // Resetting input
+    newTodoInput.value = '';
     // Creating user Object
     const newTodo = {
         todo: todoInput,
@@ -13,11 +20,18 @@ const createNewTodo = () => {
         isEditing: false,
         isComplete: false
     };
-    // Appending new todo to html ul element
-    todoList.append = `
-        <li class="todo-list__item todo">${newTodo.todo}</li>
-    `;
-
-}
+    console.log(newTodo.todo);
+    // Creating todo html element
+    const todoEl = document.createElement('li');
+    todoEl.classList.add('todo');
+    todoEl.innerText = newTodo.todo;
+    const todoSpanDel = document.createElement('span');
+    todoSpanDel.classList.add('todo__icon', 'todo__icon--del');
+    const todoSpanEdit = document.createElement('span');
+    todoSpanEdit.classList.add('todo__icon', 'todo__icon--edit');
+    todoEl.appendChild(todoSpanEdit);
+    // Adding todo to todolist
+    todoList.appendChild(todoEl);
+};
 
 handleTodoSubmit.addEventListener('click', createNewTodo);
